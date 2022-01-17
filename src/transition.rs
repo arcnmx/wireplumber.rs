7526461a31b75a5508050c59671802e9996cbb0d
arcnmx <arcnmx@users.noreply.github.com>
2022-01-16 19:26:06 -0800

    fix Transition::return_error signature

--- src/transition.rs
+++ src/transition.rs
@@ -1,13 +1,13 @@
 use glib::translate::ToGlibPtr;
-use glib::IsA;
+use glib::{IsA, Error};
 use crate::Transition;
 
 pub trait TransitionExt2: 'static {
-	fn return_error(&self, error: &mut glib::Error);
+	fn return_error(&self, error: Error);
 }
 
 impl<O: IsA<Transition>> TransitionExt2 for O {
-	fn return_error(&self, error: &mut glib::Error) {
+	fn return_error(&self, error: Error) {
 		unsafe {
 			ffi::wp_transition_return_error(self.as_ref().to_glib_none().0, error.to_glib_full() as *mut _);
 		}
