e5693d0b920dd17bfbdef621b7c504d0f775a5a5
ghost <ghost@konpaku.2hu>
2022-01-25 01:33:48 +0000

    ffabc00eda4643953a7d70c0da7aa37e644c3c0b

---
+++ wireplumber/registry/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["ConstraintType",""],["ConstraintVerb",""]],"struct":[["Constraint",""],["Interest",""],["InterestMatch",""],["InterestMatchFlags",""],["ObjectInterest",""],["ObjectManager",""]],"trait":[["InterestContainer",""]]});
