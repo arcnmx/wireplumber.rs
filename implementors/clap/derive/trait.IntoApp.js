0b854f72199e90209ba150bf86a3810967340cbe
ghost <ghost@konpaku.2hu>
2022-03-14 00:59:25 +0000

    818dacd69f74cfd1dfb0c9f2a962b94571f07e8c

--- implementors/clap/derive/trait.IntoApp.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["wpexec"] = [{"text":"impl IntoApp for <a class=\"struct\" href=\"wpexec/struct.Args.html\" title=\"struct wpexec::Args\">Args</a>","synthetic":false,"types":["wpexec::Args"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
