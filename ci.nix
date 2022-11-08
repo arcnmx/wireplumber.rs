7b7d0a6a6da15fdc52e604bcc7dd1a45545c3102
arcnmx <arcnmx@users.noreply.github.com>
2022-11-08 12:34:12 -0800

    flake.nix

--- ci.nix
+++ ci.nix
@@ -1,90 +1,26 @@
 { config, channels, pkgs, env, lib, ... }: with pkgs; with lib; let
-  importShell = config: writeText "shell.nix" ''
-    import ${builtins.unsafeDiscardStringContext config.shell.drvPath}
-  '';
-  versionFeature =
-    if versionAtLeast wireplumber.version "0.4.10" then "--features v0_4_10"
-    else if versionAtLeast wireplumber.version "0.4.8" then "--features v0_4_8"
-    else if versionAtLeast wireplumber.version "0.4.6" then "--features v0_4_6"
-    else if versionAtLeast wireplumber.version "0.4.3" then "--features v0_4_3"
-    else "";
-  cargo = config: name: command: ci.command {
+  lockData = builtins.fromJSON (builtins.readFile ./flake.lock);
+  sourceInfo = lockData.nodes.std.locked;
+  src = fetchTarball {
+    url = "https://github.com/${sourceInfo.owner}/${sourceInfo.repo}/archive/${sourceInfo.rev}.tar.gz";
+    sha256 = sourceInfo.narHash;
+  };
+  inherit (import src) Flake;
+  wireplumber-rust = Flake.CallDir ./. (Flake.Lock.Node.inputs (Flake.Lock.root (Flake.Lock.New (lockData // {
+    override.sources.nixpkgs = pkgs.path;
+  }))));
+  inherit (wireplumber-rust.packages.${system}) wpexec;
+  versionFeature = toString (mapNullable (f: "--features ${f}") (wireplumber-rust.lib.featureForVersion wireplumber.version));
+  cargo-bin = config: "${if config.enableDocs then rustChannel.buildChannel.cargo else pkgs.cargo}/bin/cargo";
+  cargo = config: name: command: args: ci.command ({
     name = "cargo-${name}";
-    command = ''
-      nix-shell ${importShell config} --run ${escapeShellArg ("cargo " + command)}
-    '';
+    command = "${cargo-bin config} " + command;
     impure = true;
-  };
-  gir-rs = pkgs.rustPlatform.buildRustPackage rec {
-    inherit (pkgs.gir-rs) postPatch meta pname;
-    version = "unstable-2022-01-24";
-
-    src = fetchFromGitHub {
-      owner = "gtk-rs";
-      repo = "gir";
-      rev = "e0d8d8d645b10561f307eabd3160b292bc423e0f";
-      sha256 = "1sg6pcmj1z0gmarh0mfwi9wiqdzk3bx7k5w8wb4q2mgrd0nipbdh";
-    };
-
-    cargoSha256 = "0bis550xcibrd3464j2hw7l0z6cfks93h910dsh0vfixpflafx79";
-    buildType = "debug";
-    doCheck = false;
-  };
-
-  wireplumber-gir = runCommand "wireplumber.gir" {
-    girName = "share/gir-1.0/Wp-${versions.majorMinor wireplumber.version}.gir";
-    wireplumber = wireplumber.dev;
-    nativeBuildInputs = [ xmlstarlet ];
-  } ''
-    WIREPLUMBER_DIR=$wireplumber/$girName
-    mkdir -p $out/$(dirname $girName)
-    # note: actually a pw_permission is 2x uint32
-    xmlstarlet ed \
-      -i '///_:type[not(@name) and @c:type="pw_permission"]' -t attr -n name -v guint64 \
-      -u '///_:constant[@c:type="WP_LOG_LEVEL_TRACE"]/@value' -v $((1<<8)) \
-      -u '///_:constant[@c:type="WP_PIPEWIRE_OBJECT_FEATURES_ALL"]/@value' -v $((992|17)) \
-      -i '///_:record[@c:type="WpIteratorMethods"]' -t attr -n glib:get-type -v wp_iterator_methods_get_type \
-      -u '///_:record[@c:type="WpSpaPod"]/_:method[@c:identifier="wp_spa_pod_get_control"]//_:parameter[@name="ctl_type"]/@transfer-ownership' -v none \
-      -u '///_:record[@c:type="WpSpaPod"]/_:method[@c:identifier="wp_spa_pod_get_property"]//_:parameter[@name="key"]/@transfer-ownership' -v none \
-      -u '///_:record[@c:type="WpSpaPod"]/_:method[@c:identifier="wp_spa_pod_get_property"]//_:parameter[@name="value"]/@transfer-ownership' -v none \
-      -u '///_:record[@c:type="WpSpaPod"]/_:method[@c:identifier="wp_spa_pod_get_string"]//_:parameter[@name="value"]/@transfer-ownership' -v none \
-      -i '///_:record[@c:type="WpSpaJson"]' -t attr -n version -v 0.4.8 \
-      -i '///_:record[@c:type="WpSpaJsonParser"]' -t attr -n version -v 0.4.8 \
-      -i '///_:record[@c:type="WpSpaJsonBuilder"]' -t attr -n version -v 0.4.8 \
-      -u '//_:namespace[@name="Wp"]/@shared-library' -v wireplumber-0.4.so.0 \
-      -i '/_:repository/_:namespace' -t elem -n package \
-      "$wireplumber/$girName" > $out/$girName
-    xmlstarlet ed -L \
-      -i '/_:repository/_:package' -t attr -n name -v wireplumber-0.4 \
-      $out/$girName
-  '';
-  gir-dirs = concatMapStringsSep " " (dev: "--girs-directories ${dev}/share/gir-1.0") [ wireplumber-gir gobject-introspection.dev ];
-  gir = writeShellScriptBin "gir" ''
-    ${gir-rs}/bin/gir ${gir-dirs} "$@"
-    if [[ $# -eq 0 ]]; then
-      if [[ -d src/auto ]]; then
-        sed -i -e '/^\/\/ from \/nix/d' src/auto/*.rs
-      elif [[ -f tests/abi.rs ]]; then
-        sed -i -e '/^\/\/ from \/nix/d' build{,_version}.rs {src,tests}/*.rs tests/*.{h,c}
-      fi
-    fi
-  '';
-  todo = writeShellScriptBin "todo" ''
-    cd ${toString ./.}
-    exec ${gir}/bin/gir -m not_bound
-  '';
-  RUSTDOCFLAGS = concatLists (mapAttrsToList (crate: url: [ "--extern-html-root-url" "${crate}=${url}" ]) rec {
-    #glib = "https://gtk-rs.org/gtk-rs-core/stable/latest/docs/";
-    glib = "https://gtk-rs.org/gtk-rs-core/git/docs/";
-    glib_sys = glib;
-    gio = glib;
-    gio_sys = glib;
-    gobject_sys = glib;
-    pipewire = "https://pipewire.pages.freedesktop.org/pipewire-rs/";
-    pipewire_sys = pipewire;
-    libspa = pipewire;
-    libspa_sys = pipewire;
-  }) ++ [ "-Z" "unstable-options" ];
+    inherit (wpexec) LIBCLANG_PATH BINDGEN_EXTRA_CLANG_ARGS;
+    PKG_CONFIG_PATH = makeSearchPath "lib/pkgconfig" wpexec.buildInputs;
+    "AR_${replaceStrings [ "-" ] [ "_" ] hostPlatform.config}" = "${stdenv.cc.bintools.bintools}/bin/${stdenv.cc.targetPrefix}ar";
+  } // args);
+  rustChannel = channels.rust.nightly;
 in {
   config = {
     name = "wireplumber.rs";
@@ -95,39 +31,28 @@ in {
     };
     cache.cachix.arc.enable = true;
     channels = {
-      nixpkgs = mkIf (env.platform != "impure") "22.05";
+      nixpkgs = mkIf (env.platform != "impure") "22.11";
       rust = "master";
-      arc = "master";
     };
     environment = {
       test = {
-        inherit (config.rustChannel.buildChannel) cargo;
+        inherit (pkgs) pkg-config;
+        inherit (stdenv) cc;
       };
     };
     tasks = {
       build.inputs = [
-        (cargo config "build-sys" "build -p wireplumber-sys")
-        (cargo config "test-sys" "test -p wireplumber-sys ${versionFeature}")
-        (cargo config "build" "build")
-        (cargo config "test-wp" "test ${versionFeature}")
-        (cargo config "test" "test --workspace ${versionFeature}")
-        (cargo config "workspace" "build --workspace --examples --bins")
+        (cargo config "build-sys" "build -p wireplumber-sys" { })
+        (cargo config "test-sys" "test -p wireplumber-sys ${versionFeature}" { })
+        (cargo config "build" "build" { })
+        (cargo config "test-wp" "test ${versionFeature}" { })
+        (cargo config "test" "test --workspace ${versionFeature}" { })
+        (cargo config "workspace" "build --workspace --examples --bins" { })
       ];
+      example.inputs = [ wpexec ];
     };
     jobs = {
-      docs = { config, pkgs, ... }: let
-        doc = ci.command {
-          name = "cargo-doc";
-          command = concatMapStringsSep "\n" (c: "nix-shell ${importShell config} --run ${escapeShellArg c}") [
-            "cargo clean --doc && rm -rf \${CARGO_TARGET_DIR:-target}/${config.rustChannel.hostTarget.triple}/doc" # `cargo clean --doc` does nothing afaict?
-            "cargo doc --no-deps -p glib-signal" # can't pass --features because cargo is garbage :<
-            "cargo doc --no-deps --workspace --all-features"
-            "cargo doc --no-deps --workspace --examples --document-private-items --all-features"
-          ];
-          docsDep = config.tasks.docs-all.drv;
-          impure = true;
-        };
-      in {
+      docs = { config, pkgs, ... }: {
         ci.gh-actions = {
           checkoutOptions = {
             fetch-depth = 0;
@@ -136,9 +61,21 @@ in {
         enableDocs = true;
         tasks = mkForce {
           docs-all.inputs = [
-            (cargo config "doc-all" "doc --all-features --workspace --no-deps")
+            (cargo config "doc-all" "doc --all-features --workspace --no-deps" { })
           ];
-          docs.inputs = doc;
+          docs.inputs = [
+            (cargo config "doc" ("clean --doc && rm -rf \${CARGO_TARGET_DIR:-target}/${rustChannel.hostTarget.triple}/doc"
+              # `cargo clean --doc` does nothing afaict?
+              + "\n" + concatStringsSep "\n" [
+                "${cargo-bin config} doc --no-deps -p glib-signal" # can't pass --features because cargo is garbage :<
+                "${cargo-bin config} doc --no-deps --workspace --all-features"
+                "${cargo-bin config} doc --no-deps --workspace --examples --document-private-items --all-features"
+              ]
+            ) {
+              docsDep = config.tasks.docs-all.drv;
+              inherit (wireplumber-rust.devShells.${system}.plain.override { enableRustdoc = true; }) RUSTDOCFLAGS;
+            })
+          ];
           publish-docs.inputs = ci.command {
             name = "publish";
             impure = true;
@@ -160,7 +97,7 @@ in {
               git checkout --orphan pages && git rm -rf .
               git reset --hard origin/$docsBranch -- || true
               rm -rf ./*
-              cp -a ''${CARGO_TARGET_DIR:-../target}/${config.rustChannel.hostTarget.triple}/doc/* ./
+              cp -a ''${CARGO_TARGET_DIR:-../target}/${rustChannel.hostTarget.triple}/doc/* ./
 
               git add -A
               if [[ -n $(git status --porcelain) ]]; then
@@ -173,38 +110,10 @@ in {
           };
         };
       };
-      dev = { config, ... }: {
-        ci.gh-actions.emit = mkForce false;
-        channels.nixpkgs = config.parentConfig.channels.nixpkgs;
-        enableDocs = true;
-        enableDev = true;
-      };
     };
   };
 
   options = {
     enableDocs = mkEnableOption "docs generation";
-    enableDev = mkEnableOption "dev shell generation";
-    rustChannel = mkOption {
-      type = types.unspecified;
-      default = if config.enableDocs
-        then arc.pkgs.rustPlatforms.nightly.hostChannel
-        else channels.rust.stable;
-    };
-    shell = mkOption {
-      type = types.unspecified;
-      default = with pkgs; config.rustChannel.mkShell {
-        rustTools = optional config.enableDev "rust-analyzer";
-        buildInputs = [ wireplumber pipewire glib ];
-        nativeBuildInputs = [ gir todo xmlstarlet pkg-config ];
-        RUSTDOCFLAGS = optionals config.enableDocs RUSTDOCFLAGS;
-        GIR_FILE = "${wireplumber-gir}/share/gir-1.0/Wp-0.4.gir";
-        LIBCLANG_PATH = "${libclang.lib}/lib";
-        BINDGEN_EXTRA_CLANG_ARGS = [
-          "-I${stdenv.cc.cc}/lib/gcc/${stdenv.hostPlatform.config}/${stdenv.cc.cc.version}/include"
-          "-I${stdenv.cc.libc.dev}/include"
-        ];
-      };
-    };
   };
 }
---
+++ flake.nix
@@ -0,0 +1,174 @@
+{
+  description = "wireplumber rust bindings";
+  inputs = {
+    flakelib.url = "github:flakelib/fl";
+    nixpkgs = { };
+    rust = {
+      url = "github:arcnmx/nixexprs-rust";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    arc = {
+      url = "github:arcnmx/nixexprs";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = { flakelib, nixpkgs, ... }@inputs: let
+    nixlib = nixpkgs.lib;
+  in flakelib {
+    inherit inputs;
+    devShells = {
+      plain = {
+        mkShell, wpexec
+      , wireplumber, pipewire, glib
+      , pkg-config, wireplumber-gir, wpdev-gir, wpdev-todo
+      , enableRustdoc ? false
+      , enableRust ? true, cargo
+      , rustTools ? [ ]
+      }: let
+        RUSTDOCFLAGS = nixlib.concatLists (nixlib.mapAttrsToList (crate: url:
+          [ "--extern-html-root-url" "${crate}=${url}" ]
+        ) rec {
+          #glib = "https://gtk-rs.org/gtk-rs-core/stable/latest/docs/";
+          glib = "https://gtk-rs.org/gtk-rs-core/git/docs/";
+          glib_sys = glib;
+          gio = glib;
+          gio_sys = glib;
+          gobject_sys = glib;
+          pipewire = "https://pipewire.pages.freedesktop.org/pipewire-rs/";
+          pipewire_sys = pipewire;
+          libspa = pipewire;
+          libspa_sys = pipewire;
+        }) ++ [ "-Z" "unstable-options" ];
+      in mkShell {
+        inherit rustTools;
+        buildInputs = [ wireplumber pipewire glib ];
+        nativeBuildInputs = [ pkg-config wpdev-gir wpdev-todo ] ++ nixlib.optional enableRust cargo;
+        RUSTDOCFLAGS = nixlib.optionals enableRustdoc RUSTDOCFLAGS;
+        GIR_FILE = "${wireplumber-gir}/share/gir-1.0/Wp-0.4.gir";
+        inherit (wpexec) LIBCLANG_PATH BINDGEN_EXTRA_CLANG_ARGS;
+      };
+      stable = { rust'stable, outputs'devShells'plain }: outputs'devShells'plain.override {
+        inherit (rust'stable) mkShell;
+        enableRust = false;
+      };
+      dev = { arc'rustPlatforms, outputs'devShells'plain }: outputs'devShells'plain.override {
+        inherit (arc'rustPlatforms.nightly.hostChannel) mkShell;
+        enableRust = false;
+        enableRustdoc = true;
+        rustTools = [ "rust-analyzer" ];
+      };
+      default = { outputs'devShells }: outputs'devShells.plain;
+    };
+    packages = {
+      wpexec = { stdenv, rustPlatform, lib, wireplumber, pipewire, glib, pkg-config, libclang }: rustPlatform.buildRustPackage rec {
+        pname = "wpexec-rs";
+        version = inputs.self.lastModifiedDate or "0";
+
+        src = inputs.self;
+        cargoLock = {
+          lockFile = ./Cargo.lock;
+          outputHashes = let
+            gtkrs = "sha256-uRdXZxV5Rfg5NXcBRj+MkSrUwnzxccyria3RuYs4YC0=";
+          in {
+            "gio-0.16.0" = gtkrs;
+            "glib-0.16.0" = gtkrs;
+            "glib-signal-0.1.0" = "sha256-rrpMbi2PzgeK+0lEMQ39v0weuqKAiSOvAyXKnhNkCGE=";
+          };
+        };
+
+        buildInputs = [ wireplumber pipewire glib ];
+        nativeBuildInputs = [ pkg-config ];
+
+        cargoBuildFlags = "-p wp-examples --bin wpexec";
+        cargoBuildFeatures = [ (lib.featureForVersion wireplumber.version) ];
+
+        buildType = "debug";
+        doCheck = false;
+
+        LIBCLANG_PATH = "${libclang.lib}/lib";
+        BINDGEN_EXTRA_CLANG_ARGS = [
+          "-I${stdenv.cc.cc}/lib/gcc/${stdenv.hostPlatform.config}/${stdenv.cc.cc.version}/include"
+          "-I${stdenv.cc.libc.dev}/include"
+        ];
+      };
+      gir-rs-0_16 = { rustPlatform, gir-rs, fetchFromGitHub }: rustPlatform.buildRustPackage rec {
+        inherit (gir-rs) postPatch meta pname;
+        version = "unstable-2022-01-24";
+
+        src = fetchFromGitHub {
+          owner = "gtk-rs";
+          repo = "gir";
+          rev = "e0d8d8d645b10561f307eabd3160b292bc423e0f";
+          sha256 = "1sg6pcmj1z0gmarh0mfwi9wiqdzk3bx7k5w8wb4q2mgrd0nipbdh";
+        };
+
+        cargoSha256 = "0bis550xcibrd3464j2hw7l0z6cfks93h910dsh0vfixpflafx79";
+        buildType = "debug";
+        doCheck = false;
+      };
+      wireplumber-gir = { runCommand, xmlstarlet, wireplumber }: runCommand "wireplumber.gir" {
+        girName = "share/gir-1.0/Wp-${nixlib.versions.majorMinor wireplumber.version}.gir";
+        wireplumber = wireplumber.dev;
+        nativeBuildInputs = [ xmlstarlet ];
+        # note: a pw_permission is actually 2x uint32
+      } ''
+        mkdir -p $out/$(dirname $girName)
+        xmlstarlet ed \
+          -i '///_:type[not(@name) and @c:type="pw_permission"]' -t attr -n name -v guint64 \
+          -u '///_:constant[@c:type="WP_LOG_LEVEL_TRACE"]/@value' -v $((1<<8)) \
+          -u '///_:constant[@c:type="WP_PIPEWIRE_OBJECT_FEATURES_ALL"]/@value' -v $((992|17)) \
+          -i '///_:record[@c:type="WpIteratorMethods"]' -t attr -n glib:get-type -v wp_iterator_methods_get_type \
+          -u '///_:record[@c:type="WpSpaPod"]/_:method[@c:identifier="wp_spa_pod_get_control"]//_:parameter[@name="ctl_type"]/@transfer-ownership' -v none \
+          -u '///_:record[@c:type="WpSpaPod"]/_:method[@c:identifier="wp_spa_pod_get_property"]//_:parameter[@name="key"]/@transfer-ownership' -v none \
+          -u '///_:record[@c:type="WpSpaPod"]/_:method[@c:identifier="wp_spa_pod_get_property"]//_:parameter[@name="value"]/@transfer-ownership' -v none \
+          -u '///_:record[@c:type="WpSpaPod"]/_:method[@c:identifier="wp_spa_pod_get_string"]//_:parameter[@name="value"]/@transfer-ownership' -v none \
+          -i '///_:record[@c:type="WpSpaJson"]' -t attr -n version -v 0.4.8 \
+          -i '///_:record[@c:type="WpSpaJsonParser"]' -t attr -n version -v 0.4.8 \
+          -i '///_:record[@c:type="WpSpaJsonBuilder"]' -t attr -n version -v 0.4.8 \
+          -u '//_:namespace[@name="Wp"]/@shared-library' -v wireplumber-0.4.so.0 \
+          -i '/_:repository/_:namespace' -t elem -n package \
+          "$wireplumber/$girName" > $out/$girName
+        xmlstarlet ed -L \
+          -i '/_:repository/_:package' -t attr -n name -v wireplumber-0.4 \
+          $out/$girName
+      '';
+    };
+    legacyPackages = { callPackageSet }: callPackageSet {
+      wpdev-gir = { writeShellScriptBin, gir-rs-0_16, wireplumber-gir, gobject-introspection }: let
+        gir-dirs = nixlib.concatMapStringsSep " " (dev:
+          "--girs-directories ${dev}/share/gir-1.0"
+        ) [ wireplumber-gir gobject-introspection.dev ];
+      in writeShellScriptBin "gir" ''
+        ${nixlib.getExe gir-rs-0_16} ${gir-dirs} "$@"
+        if [[ $# -eq 0 ]]; then
+          if [[ -d src/auto ]]; then
+            sed -i -e '/^\/\/ from \/nix/d' src/auto/*.rs
+          elif [[ -f tests/abi.rs ]]; then
+            sed -i -e '/^\/\/ from \/nix/d' build{,_version}.rs {src,tests}/*.rs tests/*.{h,c}
+          fi
+        fi
+      '';
+
+      wpdev-todo = { writeShellScriptBin, wpdev-gir }: writeShellScriptBin "todo" ''
+        cd ${toString ./.}
+        exec ${nixlib.getExe wpdev-gir} -m not_bound
+      '';
+    } { };
+    lib = with nixlib; {
+      featureForVersion = version:
+        if versionAtLeast version "0.4.10" then "v0_4_10"
+        else if versionAtLeast version "0.4.8" then "v0_4_8"
+        else if versionAtLeast version "0.4.6" then "v0_4_6"
+        else if versionAtLeast version "0.4.3" then "v0_4_3"
+        else null;
+    };
+    config = rec {
+      name = "wireplumber-rust";
+      packages.namespace = [ name ];
+      inputs.arc = {
+        lib.namespace = [ "arc" ];
+        packages.namespace = [ "arc" ];
+      };
+    };
+  };
+}
