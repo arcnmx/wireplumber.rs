e3e60c3fec5cf27db505f10c88d6678acefa1af4
ghost <ghost@konpaku.2hu>
2022-11-08 22:15:52 +0000

    4c4dd5b8cb42aa8713997c0c9d07e4d277b7d1da

--- implementors/clap/derive/trait.ValueEnum.js
+++ implementors/clap/derive/trait.ValueEnum.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"wpexec":[["impl ArgEnum for <a class=\"enum\" href=\"wpexec/enum.ModuleType.html\" title=\"enum wpexec::ModuleType\">ModuleType</a>"]]
+"wpexec":[["impl ValueEnum for <a class=\"enum\" href=\"wpexec/enum.ModuleType.html\" title=\"enum wpexec::ModuleType\">ModuleType</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
