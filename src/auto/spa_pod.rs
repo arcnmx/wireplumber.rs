arcnmx/wireplumber.rs

commit 73bc32ee27760dc85955195ba133332404e44ace

clean up generated src a bit

Gir.toml
@@ -17,8 +17,7 @@ generate = [
 	"Wp.Plugin", "Wp.ComponentLoader",
 	"Wp.SessionItem", "Wp.SessionItemFeatures",
 	"Wp.SiLink", "Wp.SiLinkable", "Wp.SiFactory",
-	"Wp.SpaDevice", "Wp.SpaDeviceFeatures", "Wp.SpaPodBuilder",
-	"Wp.SpaJson", "Wp.SpaJsonBuilder", "Wp.SpaJsonParser",
+	"Wp.SpaDevice", "Wp.SpaDeviceFeatures",
 	"Wp.State",
 	"Wp.Dbus", "Wp.DbusFeatures",
 	"Wp.ProxyFeatures",
@@ -97,6 +96,12 @@ name = "Wp.Properties"
 status = "generate"
 	[[object.derive]]
 	name = "PartialEq, Eq, PartialOrd, Ord, Hash" # defaults excluding Debug
+	[[object.function]]
+	pattern = "(.*valist|new|add_keys|add_keys_from_dict|setf|update_keys|update_keys_from_dict)"
+	ignore = true
+	[[object.function]]
+	pattern = "(new_copy|new_copy_dict|new_take|new_wrap|new_wrap_dict|add_from_dict|peek_dict|to_pw_properties|unref_and_take_pw_properties)"
+	manual = true
 
 [[object]]
 name = "Wp.PropertiesItem"
@@ -180,3 +185,56 @@ version = "" # fails to parse, so removes cfg
 	# instead, apply the version cfg to all defined bits instead
 	pattern = ".*"
 	version = "0.4.11"
+
+# unsupported methods (mostly varargs fns)
+
+[[object]]
+name = "Wp.SpaJson"
+status = "generate"
+	[[object.function]]
+	pattern = "(.*valist|new_array|new_object|object_get|parse_array|parse_object)"
+	ignore = true
+	[[object.function]]
+	pattern = "(new_wrap|spa_json)"
+	manual = true
+
+[[object]]
+name = "Wp.SpaJsonParser"
+status = "generate"
+	[[object.function]]
+	pattern = "(get|get_valist)"
+	ignore = true
+
+[[object]]
+name = "Wp.SpaJsonBuilder"
+status = "generate"
+	[[object.function]]
+	pattern = "(add|add_valist)"
+	ignore = true
+
+[[object]]
+name = "Wp.SpaPod"
+status = "generate"
+	[[object.function]]
+	pattern = "(.*valist|new_choice|new_object|new_sequence|object)"
+	ignore = true
+	[[object.function]]
+	pattern = "(new_bytes|new_pointer|new_wrap|new_wrap_const|get_bytes|get_choice_type|get_pointer|get_spa_pod|set_pointer)"
+	manual = true
+
+[[object]]
+name = "Wp.SpaPodParser"
+status = "generate"
+	[[object.function]]
+	pattern = "(get|get_bytes|get_pointer|get_valist)"
+	ignore = true
+	[[object.function]]
+	pattern = "(new_object)"
+	manual = true
+
+[[object]]
+name = "Wp.SpaPodBuilder"
+status = "generate"
+	[[object.function]]
+	pattern = "(add|add_bytes|add_pointer|add_valist)"
+	ignore = true

src/auto/spa_pod.rs
@@ -2,6 +2,7 @@
 // DO NOT EDIT
 
 use crate::Iterator;
+use crate::SpaType;
 use glib::translate::*;
 use std::mem;
 use std::ptr;
@@ -24,21 +25,6 @@ impl SpaPod {
             from_glib_full(ffi::wp_spa_pod_new_boolean(value.into_glib()))
         }
     }
-
-    //#[doc(alias = "wp_spa_pod_new_bytes")]
-    //pub fn new_bytes(value: /*Unimplemented*/Option<Basic: Pointer>, len: u32) -> SpaPod {
-    //    unsafe { TODO: call ffi:wp_spa_pod_new_bytes() }
-    //}
-
-    //#[doc(alias = "wp_spa_pod_new_choice")]
-    //pub fn new_choice(choice_type: &str, : /*Unknown conversion*//*Unimplemented*/Basic: VarArgs) -> SpaPod {
-    //    unsafe { TODO: call ffi:wp_spa_pod_new_choice() }
-    //}
-
-    //#[doc(alias = "wp_spa_pod_new_choice_valist")]
-    //pub fn new_choice_valist(choice_type: &str, args: /*Unknown conversion*//*Unimplemented*/Unsupported) -> SpaPod {
-    //    unsafe { TODO: call ffi:wp_spa_pod_new_choice_valist() }
-    //}
 
     #[doc(alias = "wp_spa_pod_new_double")]
     pub fn new_double(value: f64) -> SpaPod {
@@ -96,21 +82,6 @@ impl SpaPod {
         }
     }
 
-    //#[doc(alias = "wp_spa_pod_new_object")]
-    //pub fn new_object(type_name: &str, id_name: &str, : /*Unknown conversion*//*Unimplemented*/Basic: VarArgs) -> SpaPod {
-    //    unsafe { TODO: call ffi:wp_spa_pod_new_object() }
-    //}
-
-    //#[doc(alias = "wp_spa_pod_new_object_valist")]
-    //pub fn new_object_valist(type_name: &str, id_name: &str, args: /*Unknown conversion*//*Unimplemented*/Unsupported) -> SpaPod {
-    //    unsafe { TODO: call ffi:wp_spa_pod_new_object_valist() }
-    //}
-
-    //#[doc(alias = "wp_spa_pod_new_pointer")]
-    //pub fn new_pointer(type_name: &str, value: /*Unimplemented*/Option<Basic: Pointer>) -> SpaPod {
-    //    unsafe { TODO: call ffi:wp_spa_pod_new_pointer() }
-    //}
-
     #[doc(alias = "wp_spa_pod_new_rectangle")]
     pub fn new_rectangle(width: u32, height: u32) -> SpaPod {
         unsafe {
@@ -118,32 +89,12 @@ impl SpaPod {
         }
     }
 
-    //#[doc(alias = "wp_spa_pod_new_sequence")]
-    //pub fn new_sequence(unit: u32, : /*Unknown conversion*//*Unimplemented*/Basic: VarArgs) -> SpaPod {
-    //    unsafe { TODO: call ffi:wp_spa_pod_new_sequence() }
-    //}
-
-    //#[doc(alias = "wp_spa_pod_new_sequence_valist")]
-    //pub fn new_sequence_valist(unit: u32, args: /*Unknown conversion*//*Unimplemented*/Unsupported) -> SpaPod {
-    //    unsafe { TODO: call ffi:wp_spa_pod_new_sequence_valist() }
-    //}
-
     #[doc(alias = "wp_spa_pod_new_string")]
     pub fn new_string(value: &str) -> SpaPod {
         unsafe {
             from_glib_full(ffi::wp_spa_pod_new_string(value.to_glib_none().0))
         }
     }
-
-    //#[doc(alias = "wp_spa_pod_new_wrap")]
-    //pub fn new_wrap(pod: /*Unimplemented*/Option<Basic: Pointer>) -> SpaPod {
-    //    unsafe { TODO: call ffi:wp_spa_pod_new_wrap() }
-    //}
-
-    //#[doc(alias = "wp_spa_pod_new_wrap_const")]
-    //pub fn new_wrap_const(pod: /*Unimplemented*/Option<Basic: Pointer>) -> SpaPod {
-    //    unsafe { TODO: call ffi:wp_spa_pod_new_wrap_const() }
-    //}
 
     #[doc(alias = "wp_spa_pod_copy")]
 #[must_use]
@@ -202,12 +153,6 @@ impl SpaPod {
         }
     }
 
-    //#[doc(alias = "wp_spa_pod_get_bytes")]
-    //#[doc(alias = "get_bytes")]
-    //pub fn bytes(&self, value: /*Unimplemented*/&mut Option<Basic: Pointer>) -> Option<u32> {
-    //    unsafe { TODO: call ffi:wp_spa_pod_get_bytes() }
-    //}
-
     #[doc(alias = "wp_spa_pod_get_choice_child")]
     #[doc(alias = "get_choice_child")]
 #[must_use]
@@ -216,12 +161,6 @@ impl SpaPod {
             from_glib_full(ffi::wp_spa_pod_get_choice_child(self.to_glib_none().0))
         }
     }
-
-    //#[doc(alias = "wp_spa_pod_get_choice_type")]
-    //#[doc(alias = "get_choice_type")]
-    //pub fn choice_type(&self) -> /*Unimplemented*/Option<SpaIdValue> {
-    //    unsafe { TODO: call ffi:wp_spa_pod_get_choice_type() }
-    //}
 
     #[doc(alias = "wp_spa_pod_get_control")]
     #[doc(alias = "get_control")]
@@ -312,18 +251,6 @@ impl SpaPod {
     //    unsafe { TODO: call ffi:wp_spa_pod_get_object() }
     //}
 
-    //#[doc(alias = "wp_spa_pod_get_object_valist")]
-    //#[doc(alias = "get_object_valist")]
-    //pub fn object_valist(&self, args: /*Unknown conversion*//*Unimplemented*/&mut Unsupported) -> Option<glib::GString> {
-    //    unsafe { TODO: call ffi:wp_spa_pod_get_object_valist() }
-    //}
-
-    //#[doc(alias = "wp_spa_pod_get_pointer")]
-    //#[doc(alias = "get_pointer")]
-    //pub fn pointer(&self, value: /*Unimplemented*/&mut Option<Basic: Pointer>) -> bool {
-    //    unsafe { TODO: call ffi:wp_spa_pod_get_pointer() }
-    //}
-
     #[doc(alias = "wp_spa_pod_get_property")]
     #[doc(alias = "get_property")]
     pub fn property(&self) -> Option<(glib::GString, SpaPod)> {
@@ -346,11 +273,13 @@ impl SpaPod {
         }
     }
 
-    //#[doc(alias = "wp_spa_pod_get_spa_pod")]
-    //#[doc(alias = "get_spa_pod")]
-    //pub fn spa_pod(&self) -> /*Unimplemented*/Option<Basic: Pointer> {
-    //    unsafe { TODO: call ffi:wp_spa_pod_get_spa_pod() }
-    //}
+    #[doc(alias = "wp_spa_pod_get_spa_type")]
+    #[doc(alias = "get_spa_type")]
+    pub fn spa_type(&self) -> Option<SpaType> {
+        unsafe {
+            from_glib(ffi::wp_spa_pod_get_spa_type(self.to_glib_none().0))
+        }
+    }
 
     #[doc(alias = "wp_spa_pod_get_string")]
     #[doc(alias = "get_string")]
@@ -366,12 +295,6 @@ impl SpaPod {
     //#[doc(alias = "get_struct")]
     //pub fn is_struct(&self, : /*Unknown conversion*//*Unimplemented*/Basic: VarArgs) -> bool {
     //    unsafe { TODO: call ffi:wp_spa_pod_get_struct() }
-    //}
-
-    //#[doc(alias = "wp_spa_pod_get_struct_valist")]
-    //#[doc(alias = "get_struct_valist")]
-    //pub fn struct_valist(&self, args: /*Unknown conversion*//*Unimplemented*/&mut Unsupported) -> bool {
-    //    unsafe { TODO: call ffi:wp_spa_pod_get_struct_valist() }
     //}
 
     #[doc(alias = "wp_spa_pod_is_array")]
@@ -590,11 +513,6 @@ impl SpaPod {
             from_glib(ffi::wp_spa_pod_set_pod(self.to_glib_none().0, pod.to_glib_none().0))
         }
     }
-
-    //#[doc(alias = "wp_spa_pod_set_pointer")]
-    //pub fn set_pointer(&self, type_name: &str, value: /*Unimplemented*/Option<Basic: Pointer>) -> bool {
-    //    unsafe { TODO: call ffi:wp_spa_pod_set_pointer() }
-    //}
 
     #[doc(alias = "wp_spa_pod_set_rectangle")]
     pub fn set_rectangle(&self, width: u32, height: u32) -> bool {

src/spa/pod.rs
@@ -2,7 +2,7 @@ use libspa_sys::{spa_pod, spa_rectangle, spa_fraction};
 use crate::pw::SpaPropertyKey;
 #[cfg(any(feature = "experimental", feature = "dox"))]
 use crate::pw::PipewireObject;
-use crate::spa::{SpaPod, SpaType, SpaIdValue, SpaPodParser, SpaPodBuilder, SpaPrimitive, SpaValue};
+use crate::spa::{SpaPod, SpaIdValue, SpaPodParser, SpaPodBuilder, SpaPrimitive, SpaValue};
 use crate::prelude::*;
 
 impl SpaPod {
@@ -74,13 +74,6 @@ impl SpaPod {
 	pub fn to_bytes(&self) -> Vec<u8> {
 		unsafe {
 			self.as_bytes().into()
-		}
-	}
-
-	#[doc(alias = "get_spa_type")]
-	pub fn spa_type(&self) -> Option<SpaType> {
-		unsafe {
-			from_glib(ffi::wp_spa_pod_get_spa_type(self.to_glib_none().0))
 		}
 	}
 

src/auto/properties.rs
@@ -16,21 +16,6 @@ glib::wrapper! {
 }
 
 impl Properties {
-    //#[doc(alias = "wp_properties_new")]
-    //pub fn new(key: &str, : /*Unknown conversion*//*Unimplemented*/Basic: VarArgs) -> Properties {
-    //    unsafe { TODO: call ffi:wp_properties_new() }
-    //}
-
-    //#[doc(alias = "wp_properties_new_copy")]
-    //pub fn new_copy(props: /*Unimplemented*/Option<Basic: Pointer>) -> Properties {
-    //    unsafe { TODO: call ffi:wp_properties_new_copy() }
-    //}
-
-    //#[doc(alias = "wp_properties_new_copy_dict")]
-    //pub fn new_copy_dict(dict: /*Unimplemented*/Option<Basic: Pointer>) -> Properties {
-    //    unsafe { TODO: call ffi:wp_properties_new_copy_dict() }
-    //}
-
     #[doc(alias = "wp_properties_new_empty")]
     pub fn new_empty() -> Properties {
         unsafe {
@@ -45,26 +30,6 @@ impl Properties {
         }
     }
 
-    //#[doc(alias = "wp_properties_new_take")]
-    //pub fn new_take(props: /*Unimplemented*/Option<Basic: Pointer>) -> Properties {
-    //    unsafe { TODO: call ffi:wp_properties_new_take() }
-    //}
-
-    //#[doc(alias = "wp_properties_new_valist")]
-    //pub fn new_valist(key: &str, args: /*Unknown conversion*//*Unimplemented*/Unsupported) -> Properties {
-    //    unsafe { TODO: call ffi:wp_properties_new_valist() }
-    //}
-
-    //#[doc(alias = "wp_properties_new_wrap")]
-    //pub fn new_wrap(props: /*Unimplemented*/Option<Basic: Pointer>) -> Properties {
-    //    unsafe { TODO: call ffi:wp_properties_new_wrap() }
-    //}
-
-    //#[doc(alias = "wp_properties_new_wrap_dict")]
-    //pub fn new_wrap_dict(dict: /*Unimplemented*/Option<Basic: Pointer>) -> Properties {
-    //    unsafe { TODO: call ffi:wp_properties_new_wrap_dict() }
-    //}
-
     #[doc(alias = "wp_properties_add")]
     pub fn add(&self, props: &Properties) -> i32 {
         unsafe {
@@ -72,27 +37,12 @@ impl Properties {
         }
     }
 
-    //#[doc(alias = "wp_properties_add_from_dict")]
-    //pub fn add_from_dict(&self, dict: /*Unimplemented*/Option<Basic: Pointer>) -> i32 {
-    //    unsafe { TODO: call ffi:wp_properties_add_from_dict() }
-    //}
-
-    //#[doc(alias = "wp_properties_add_keys")]
-    //pub fn add_keys(&self, props: &Properties, key1: &str, : /*Unknown conversion*//*Unimplemented*/Basic: VarArgs) -> i32 {
-    //    unsafe { TODO: call ffi:wp_properties_add_keys() }
-    //}
-
     #[doc(alias = "wp_properties_add_keys_array")]
     pub fn add_keys_array(&self, props: &Properties, keys: &[&str]) -> i32 {
         unsafe {
             ffi::wp_properties_add_keys_array(self.to_glib_none().0, props.to_glib_none().0, keys.to_glib_none().0)
         }
     }
-
-    //#[doc(alias = "wp_properties_add_keys_from_dict")]
-    //pub fn add_keys_from_dict(&self, dict: /*Unimplemented*/Option<Basic: Pointer>, key1: &str, : /*Unknown conversion*//*Unimplemented*/Basic: VarArgs) -> i32 {
-    //    unsafe { TODO: call ffi:wp_properties_add_keys_from_dict() }
-    //}
 
     #[doc(alias = "wp_properties_copy")]
 #[must_use]
@@ -141,11 +91,6 @@ impl Properties {
         }
     }
 
-    //#[doc(alias = "wp_properties_peek_dict")]
-    //pub fn peek_dict(&self) -> /*Unimplemented*/Option<Basic: Pointer> {
-    //    unsafe { TODO: call ffi:wp_properties_peek_dict() }
-    //}
-
     #[doc(alias = "wp_properties_set")]
     pub fn set(&self, key: &str, value: Option<&str>) -> i32 {
         unsafe {
@@ -153,32 +98,12 @@ impl Properties {
         }
     }
 
-    //#[doc(alias = "wp_properties_setf")]
-    //pub fn setf(&self, key: &str, format: &str, : /*Unknown conversion*//*Unimplemented*/Basic: VarArgs) -> i32 {
-    //    unsafe { TODO: call ffi:wp_properties_setf() }
-    //}
-
-    //#[doc(alias = "wp_properties_setf_valist")]
-    //pub fn setf_valist(&self, key: &str, format: &str, args: /*Unknown conversion*//*Unimplemented*/Unsupported) -> i32 {
-    //    unsafe { TODO: call ffi:wp_properties_setf_valist() }
-    //}
-
     #[doc(alias = "wp_properties_sort")]
     pub fn sort(&self) {
         unsafe {
             ffi::wp_properties_sort(self.to_glib_none().0);
         }
     }
-
-    //#[doc(alias = "wp_properties_to_pw_properties")]
-    //pub fn to_pw_properties(&self) -> /*Unimplemented*/Option<Basic: Pointer> {
-    //    unsafe { TODO: call ffi:wp_properties_to_pw_properties() }
-    //}
-
-    //#[doc(alias = "wp_properties_unref_and_take_pw_properties")]
-    //pub fn unref_and_take_pw_properties(&self) -> /*Unimplemented*/Option<Basic: Pointer> {
-    //    unsafe { TODO: call ffi:wp_properties_unref_and_take_pw_properties() }
-    //}
 
     #[doc(alias = "wp_properties_update")]
     pub fn update(&self, props: &Properties) -> i32 {
@@ -192,22 +117,12 @@ impl Properties {
     //    unsafe { TODO: call ffi:wp_properties_update_from_dict() }
     //}
 
-    //#[doc(alias = "wp_properties_update_keys")]
-    //pub fn update_keys(&self, props: &Properties, key1: &str, : /*Unknown conversion*//*Unimplemented*/Basic: VarArgs) -> i32 {
-    //    unsafe { TODO: call ffi:wp_properties_update_keys() }
-    //}
-
     #[doc(alias = "wp_properties_update_keys_array")]
     pub fn update_keys_array(&self, props: &Properties, keys: &[&str]) -> i32 {
         unsafe {
             ffi::wp_properties_update_keys_array(self.to_glib_none().0, props.to_glib_none().0, keys.to_glib_none().0)
         }
     }
-
-    //#[doc(alias = "wp_properties_update_keys_from_dict")]
-    //pub fn update_keys_from_dict(&self, dict: /*Unimplemented*/Option<Basic: Pointer>, key1: &str, : /*Unknown conversion*//*Unimplemented*/Basic: VarArgs) -> i32 {
-    //    unsafe { TODO: call ffi:wp_properties_update_keys_from_dict() }
-    //}
 
     #[doc(alias = "wp_properties_iterator_item_get_key")]
     pub fn iterator_item_get_key(item: &glib::Value) -> Option<glib::GString> {

src/auto/spa_json.rs
@@ -20,16 +20,6 @@ glib::wrapper! {
 }
 
 impl SpaJson {
-    //#[doc(alias = "wp_spa_json_new_array")]
-    //pub fn new_array(format: Option<&str>, : /*Unknown conversion*//*Unimplemented*/Basic: VarArgs) -> SpaJson {
-    //    unsafe { TODO: call ffi:wp_spa_json_new_array() }
-    //}
-
-    //#[doc(alias = "wp_spa_json_new_array_valist")]
-    //pub fn new_array_valist(format: Option<&str>, args: /*Unknown conversion*//*Unimplemented*/Unsupported) -> SpaJson {
-    //    unsafe { TODO: call ffi:wp_spa_json_new_array_valist() }
-    //}
-
     #[doc(alias = "wp_spa_json_new_boolean")]
     pub fn new_boolean(value: bool) -> SpaJson {
         unsafe {
@@ -77,27 +67,12 @@ impl SpaJson {
         }
     }
 
-    //#[doc(alias = "wp_spa_json_new_object")]
-    //pub fn new_object(key: Option<&str>, format: Option<&str>, : /*Unknown conversion*//*Unimplemented*/Basic: VarArgs) -> SpaJson {
-    //    unsafe { TODO: call ffi:wp_spa_json_new_object() }
-    //}
-
-    //#[doc(alias = "wp_spa_json_new_object_valist")]
-    //pub fn new_object_valist(key: Option<&str>, format: Option<&str>, args: /*Unknown conversion*//*Unimplemented*/Unsupported) -> SpaJson {
-    //    unsafe { TODO: call ffi:wp_spa_json_new_object_valist() }
-    //}
-
     #[doc(alias = "wp_spa_json_new_string")]
     pub fn new_string(value: &str) -> SpaJson {
         unsafe {
             from_glib_full(ffi::wp_spa_json_new_string(value.to_glib_none().0))
         }
     }
-
-    //#[doc(alias = "wp_spa_json_new_wrap")]
-    //pub fn new_wrap(json: /*Unimplemented*/Option<Basic: Pointer>) -> SpaJson {
-    //    unsafe { TODO: call ffi:wp_spa_json_new_wrap() }
-    //}
 
     #[doc(alias = "wp_spa_json_copy")]
 #[must_use]
@@ -200,26 +175,6 @@ impl SpaJson {
         }
     }
 
-    //#[doc(alias = "wp_spa_json_object_get")]
-    //pub fn object_get(&self, : /*Unknown conversion*//*Unimplemented*/Basic: VarArgs) -> bool {
-    //    unsafe { TODO: call ffi:wp_spa_json_object_get() }
-    //}
-
-    //#[doc(alias = "wp_spa_json_object_get_valist")]
-    //pub fn object_get_valist(&self, args: /*Unknown conversion*//*Unimplemented*/Unsupported) -> bool {
-    //    unsafe { TODO: call ffi:wp_spa_json_object_get_valist() }
-    //}
-
-    //#[doc(alias = "wp_spa_json_parse_array")]
-    //pub fn parse_array(&self, : /*Unknown conversion*//*Unimplemented*/Basic: VarArgs) -> bool {
-    //    unsafe { TODO: call ffi:wp_spa_json_parse_array() }
-    //}
-
-    //#[doc(alias = "wp_spa_json_parse_array_valist")]
-    //pub fn parse_array_valist(&self, args: /*Unknown conversion*//*Unimplemented*/&mut Unsupported) -> bool {
-    //    unsafe { TODO: call ffi:wp_spa_json_parse_array_valist() }
-    //}
-
     #[doc(alias = "wp_spa_json_parse_boolean")]
     pub fn parse_boolean(&self) -> Option<bool> {
         unsafe {
@@ -246,16 +201,6 @@ impl SpaJson {
             if ret { Some(value.assume_init()) } else { None }
         }
     }
-
-    //#[doc(alias = "wp_spa_json_parse_object")]
-    //pub fn parse_object(&self, : /*Unknown conversion*//*Unimplemented*/Basic: VarArgs) -> bool {
-    //    unsafe { TODO: call ffi:wp_spa_json_parse_object() }
-    //}
-
-    //#[doc(alias = "wp_spa_json_parse_object_valist")]
-    //pub fn parse_object_valist(&self, args: /*Unknown conversion*//*Unimplemented*/&mut Unsupported) -> bool {
-    //    unsafe { TODO: call ffi:wp_spa_json_parse_object_valist() }
-    //}
 
     #[doc(alias = "wp_spa_json_parse_string")]
     pub fn parse_string(&self) -> Option<glib::GString> {

src/auto/spa_json_builder.rs
@@ -30,11 +30,6 @@ impl SpaJsonBuilder {
         }
     }
 
-    //#[doc(alias = "wp_spa_json_builder_add")]
-    //pub fn add(&self, : /*Unknown conversion*//*Unimplemented*/Basic: VarArgs) {
-    //    unsafe { TODO: call ffi:wp_spa_json_builder_add() }
-    //}
-
     #[doc(alias = "wp_spa_json_builder_add_boolean")]
     pub fn add_boolean(&self, value: bool) {
         unsafe {
@@ -83,11 +78,6 @@ impl SpaJsonBuilder {
             ffi::wp_spa_json_builder_add_string(self.to_glib_none().0, value.to_glib_none().0);
         }
     }
-
-    //#[doc(alias = "wp_spa_json_builder_add_valist")]
-    //pub fn add_valist(&self, args: /*Unknown conversion*//*Unimplemented*/Unsupported) {
-    //    unsafe { TODO: call ffi:wp_spa_json_builder_add_valist() }
-    //}
 
     #[doc(alias = "wp_spa_json_builder_end")]
     pub fn end(&self) -> Option<SpaJson> {

src/auto/spa_json_parser.rs
@@ -38,11 +38,6 @@ impl SpaJsonParser {
         }
     }
 
-    //#[doc(alias = "wp_spa_json_parser_get")]
-    //pub fn get(&self, : /*Unknown conversion*//*Unimplemented*/Basic: VarArgs) -> bool {
-    //    unsafe { TODO: call ffi:wp_spa_json_parser_get() }
-    //}
-
     #[doc(alias = "wp_spa_json_parser_get_boolean")]
     #[doc(alias = "get_boolean")]
     pub fn boolean(&self) -> Option<bool> {
@@ -96,10 +91,4 @@ impl SpaJsonParser {
             from_glib_full(ffi::wp_spa_json_parser_get_string(self.to_glib_none().0))
         }
     }
-
-    //#[doc(alias = "wp_spa_json_parser_get_valist")]
-    //#[doc(alias = "get_valist")]
-    //pub fn is_valist(&self, args: /*Unknown conversion*//*Unimplemented*/Unsupported) -> bool {
-    //    unsafe { TODO: call ffi:wp_spa_json_parser_get_valist() }
-    //}
 }

src/auto/spa_pod_builder.rs
@@ -51,22 +51,12 @@ impl SpaPodBuilder {
         }
     }
 
-    //#[doc(alias = "wp_spa_pod_builder_add")]
-    //pub fn add(&self, : /*Unknown conversion*//*Unimplemented*/Basic: VarArgs) {
-    //    unsafe { TODO: call ffi:wp_spa_pod_builder_add() }
-    //}
-
     #[doc(alias = "wp_spa_pod_builder_add_boolean")]
     pub fn add_boolean(&self, value: bool) {
         unsafe {
             ffi::wp_spa_pod_builder_add_boolean(self.to_glib_none().0, value.into_glib());
         }
     }
-
-    //#[doc(alias = "wp_spa_pod_builder_add_bytes")]
-    //pub fn add_bytes(&self, value: /*Unimplemented*/Option<Basic: Pointer>, len: u32) {
-    //    unsafe { TODO: call ffi:wp_spa_pod_builder_add_bytes() }
-    //}
 
     #[doc(alias = "wp_spa_pod_builder_add_control")]
     pub fn add_control(&self, offset: u32, ctl_type: &str) {
@@ -138,11 +128,6 @@ impl SpaPodBuilder {
         }
     }
 
-    //#[doc(alias = "wp_spa_pod_builder_add_pointer")]
-    //pub fn add_pointer(&self, type_name: &str, value: /*Unimplemented*/Option<Basic: Pointer>) {
-    //    unsafe { TODO: call ffi:wp_spa_pod_builder_add_pointer() }
-    //}
-
     #[doc(alias = "wp_spa_pod_builder_add_property")]
     pub fn add_property(&self, key: &str) {
         unsafe {
@@ -170,11 +155,6 @@ impl SpaPodBuilder {
             ffi::wp_spa_pod_builder_add_string(self.to_glib_none().0, value.to_glib_none().0);
         }
     }
-
-    //#[doc(alias = "wp_spa_pod_builder_add_valist")]
-    //pub fn add_valist(&self, args: /*Unknown conversion*//*Unimplemented*/Unsupported) {
-    //    unsafe { TODO: call ffi:wp_spa_pod_builder_add_valist() }
-    //}
 
     #[doc(alias = "wp_spa_pod_builder_end")]
     pub fn end(&self) -> Option<SpaPod> {

commit fb025d3e334bc452aac2db00157de307cfb39291

remove WpSpaPod Gir config
73bc32ee27760dc85955195ba133332404e44ace accidentally broke things

Gir.toml
@@ -83,13 +83,6 @@ status = "generate"
 	manual = true
 
 [[object]]
-name = "Wp.SpaPod"
-status = "generate"
-	[[object.function]]
-	name = "get_spa_type"
-	manual = true
-
-[[object]]
 name = "Wp.SpaPodParser"
 status = "generate"
 	[[object.function]]
@@ -255,7 +248,7 @@ status = "generate"
 	pattern = "(.*valist|new_choice|new_object|new_sequence|get_object|get_struct)"
 	ignore = true
 	[[object.function]]
-	pattern = "(new_bytes|new_pointer|new_wrap|new_wrap_const|get_bytes|get_choice_type|get_pointer|get_spa_pod|set_pointer)"
+	pattern = "(new_bytes|new_pointer|new_wrap|new_wrap_const|get_bytes|get_choice_type|get_pointer|get_spa_type|get_spa_pod|set_pointer)"
 	manual = true
 
 [[object]]

src/auto/spa_pod.rs
@@ -267,14 +267,6 @@ impl SpaPod {
         }
     }
 
-    #[doc(alias = "wp_spa_pod_get_spa_type")]
-    #[doc(alias = "get_spa_type")]
-    pub fn spa_type(&self) -> Option<SpaType> {
-        unsafe {
-            from_glib(ffi::wp_spa_pod_get_spa_type(self.to_glib_none().0))
-        }
-    }
-
     #[doc(alias = "wp_spa_pod_get_string")]
     #[doc(alias = "get_string")]
     pub fn string(&self) -> Option<glib::GString> {

src/spa/pod.rs
@@ -4,7 +4,7 @@ use {
 	crate::{
 		prelude::*,
 		pw::SpaPropertyKey,
-		spa::{SpaIdValue, SpaPod, SpaPodBuilder, SpaPodParser, SpaPrimitive, SpaValue},
+		spa::{SpaIdValue, SpaPod, SpaPodBuilder, SpaPodParser, SpaPrimitive, SpaType, SpaValue},
 	},
 	libspa_sys::{spa_fraction, spa_pod, spa_rectangle},
 };
@@ -98,6 +98,12 @@ impl SpaPod {
 	#[doc(alias = "wp_spa_pod_new_pointer")]
 	pub fn new_pointer(type_name: &str, value: gconstpointer) -> SpaPod {
 		unsafe { from_glib_full(ffi::wp_spa_pod_new_pointer(type_name.to_glib_none().0, value)) }
+	}
+
+	#[doc(alias = "wp_spa_pod_get_spa_type")]
+	#[doc(alias = "get_spa_type")]
+	pub fn spa_type(&self) -> Option<SpaType> {
+		unsafe { from_glib(ffi::wp_spa_pod_get_spa_type(self.to_glib_none().0)) }
 	}
 
 	#[doc(alias = "wp_spa_pod_get_bytes")]