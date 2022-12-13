961641dfcb48e4666a69d2d3614a210b6d88298c
ghost <ghost@konpaku.2hu>
2022-12-13 02:43:18 +0000

    main: 0e4830ff2f4a60c86def9417859e56232ec64969

--- main/implementors/serde/ser/trait.Error.js
+++ main/implementors/serde/ser/trait.Error.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"wireplumber":[["impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.147/serde/ser/trait.Error.html\" title=\"trait serde::ser::Error\">Error</a> for <a class=\"enum\" href=\"wireplumber/lua/enum.LuaError.html\" title=\"enum wireplumber::lua::LuaError\">LuaError</a>"]]
+"wireplumber":[["impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.150/serde/ser/trait.Error.html\" title=\"trait serde::ser::Error\">Error</a> for <a class=\"enum\" href=\"wireplumber/lua/enum.LuaError.html\" title=\"enum wireplumber::lua::LuaError\">LuaError</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- main/implementors/serde/ser/trait.Serializer.js
+++ main/implementors/serde/ser/trait.Serializer.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"wireplumber":[["impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.147/serde/ser/trait.Serializer.html\" title=\"trait serde::ser::Serializer\">Serializer</a> for <a class=\"struct\" href=\"wireplumber/lua/struct.Serializer.html\" title=\"struct wireplumber::lua::Serializer\">Serializer</a>"]]
+"wireplumber":[["impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.150/serde/ser/trait.Serializer.html\" title=\"trait serde::ser::Serializer\">Serializer</a> for <a class=\"struct\" href=\"wireplumber/lua/struct.Serializer.html\" title=\"struct wireplumber::lua::Serializer\">Serializer</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
