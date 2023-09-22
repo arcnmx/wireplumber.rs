c7190621738ec733bbab94d05a6a4a931a81c88e
ghost <ghost@konpaku.2hu>
2023-09-22 18:28:47 +0000

    main: 5b1bd09c2bcda9c7e4a807193ecf9910c8dbe757

--- main/implementors/serde/de/trait.Error.js
+++ main/implementors/serde/de/trait.Error.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"wireplumber":[["impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.183/serde/de/trait.Error.html\" title=\"trait serde::de::Error\">Error</a> for <a class=\"enum\" href=\"wireplumber/lua/enum.LuaError.html\" title=\"enum wireplumber::lua::LuaError\">LuaError</a>"]]
+"wireplumber":[["impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.185/serde/de/trait.Error.html\" title=\"trait serde::de::Error\">Error</a> for <a class=\"enum\" href=\"wireplumber/lua/enum.LuaError.html\" title=\"enum wireplumber::lua::LuaError\">LuaError</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
