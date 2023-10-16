d1c143ce9858c13987fad1488e705e915ebfa11e
ghost <ghost@konpaku.2hu>
2023-10-16 21:01:43 +0000

    main: 35357eed299477ff17f83e52ba11836d040b08e5

---
+++ main/glib_signal/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"macro":["def_signal","impl_signal"],"struct":["BorrowedObject","ConnectDetails","ConnectEof","OnceFuture","Pointer","PrimitiveValue","SignalFlags","SignalStream"],"trait":["BuildSignal","BuildableSignal","DetailedSignal","FromValues","Notifies","ObjectSignalExt","Signal","ToValueOption"]};
